c3a9dcf84b9df5ebdbff0bea7dd0d416e8845210
Phil Warner <phil@i9-12900K>
2024-01-25 09:16:18 -0500

    Add main logic.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Gathering input from user.");
+		
+		System.out.println("Processing it...");
+		
+		//do stuff
+		
+		//save to DB
+		
+		//Static reference to out tokens...
+		System.out.println("Connect... " + Secret.USER + ":" + Secret.TOKEN);
+	}
+
+}
